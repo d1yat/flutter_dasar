1ba5460f6420282da0780b444e5a030d614330cf
Akhirudin Sudiyat <me.sudiyat@gmail.com>
2025-12-21 15:23:55 +0700

    add action button

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,46 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  String _message = 'Hello World!';
+  String _buttonText = 'Click Me';
+
+  void _handleButtonClick() {
+    setState(() {
+      if (_buttonText == 'Click Me') {
+        _buttonText = 'Restart';
+        _message = 'Button pressed!';
+      } else {
+        _buttonText = 'Click Me';
+        _message = 'Hello World!';
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Dasar',
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(_message, style: TextStyle(fontSize: 20)),
+              const SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: _handleButtonClick,
+                child: Text(_buttonText),
+              ),
+            ],
+          ),
         ),
       ),
     );
